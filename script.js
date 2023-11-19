f9578548839007c761d850e91f288c8dc51dece3
Hazelrl <hazel23894@outlook.com>
2023-11-19 22:47:45 +0000

    Unnecessary files removed and misc errors fixed

--- script.js
+++ script.js
@@ -1,20 +1,62 @@
-// Get all elements with the class "badge html rounded-pill"
-var badgeElements = document.querySelectorAll('.html');
+/* SKILLS FILTER FOR PROJECTS */
 
-// Add a click event listener to each element
-badgeElements.forEach(function(badgeElement) {
-  badgeElement.addEventListener('click', function() {
-         // Get all cards
-    const allCards = document.querySelectorAll('.card');
+/* See all button */
 
-    // Iterate through each card and check if it has a descendant with class "html"
-    allCards.forEach(card => {
-        const hasHtmlDescendant = card.querySelector('.html');
+// Show all when 'See all' badge is clicked
+    // Function to remove the "hidden" class from all cards
+    function showAllCards() {
+        const allCards = document.querySelectorAll('.card');
+        allCards.forEach(card => {
+            card.classList.remove('hidden');
+        });
+    }
 
-        // If no descendant has class "html," add the class "hidden" to hide the card
-        if (!hasHtmlDescendant) {
-            card.classList.add('hidden');
+    // Add click event listener to the document body
+    document.body.addEventListener('click', function(event) {
+        // Check if the clicked element has the class "all"
+        if (event.target.classList.contains('all')) {
+            // Call the function to show all cards
+            showAllCards();
         }
     });
-  });
-});
+
+
+/* Skill-specific buttons */
+
+function hideCardsWithoutDescendant(className) {
+    // Get all elements with the specified class
+    var badgeElements = document.querySelectorAll('.' + className);
+
+    // Add a click event listener to each element
+    badgeElements.forEach(function (badgeElement) {
+        badgeElement.addEventListener('click', function () {
+            showAllCards();
+            // Get all cards
+            const allCards = document.querySelectorAll('.card');
+
+
+            // Iterate through each card and check if it has a descendant with the specified class
+            allCards.forEach(card => {
+
+                const hasDescendant = card.querySelector('.' + className);
+
+                // If no descendant has the specified class, add the class "hidden" to hide the card
+                if (!hasDescendant) {
+                    card.classList.add('hidden');
+                }
+            });
+        });
+    });
+}
+
+// Call for all skills
+hideCardsWithoutDescendant('html');
+hideCardsWithoutDescendant('css');
+hideCardsWithoutDescendant('javascript');
+hideCardsWithoutDescendant('sass');
+hideCardsWithoutDescendant('svg');
+hideCardsWithoutDescendant('api');
+
+
+
+
